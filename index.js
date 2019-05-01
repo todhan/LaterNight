c0603b545afcc850dadd9962f0978cd2b35efc8e
xiaodaor <48588460+xiaodaor@users.noreply.github.com>
2019-05-01 16:18:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,10 @@ if (typeof (Storage) !== "undefined") {
     document.getElementById("result").innerHTML = "你这个浏览器不支持 Web Storage";
 }
 
-
-
-
+var test;
 $(document).ready( function(){
-	//Random pics
+	test =function {
+			//Random pics
 var images = [
 	"https://i.loli.net/2019/03/15/5c8b0f6cb01ee.gif",
 	"https://i.loli.net/2019/03/15/5c8b0f568cacb.gif",
@@ -39,4 +38,6 @@ var images = [
 	];
 var url = Math.floor(Math.random() * images.length);
 document.getElementById('img').src = images[url];
+	}
+
 } );
