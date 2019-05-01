455d85870ec48b758ccb7afbcacc5c6841044ae7
xiaodaor <48588460+xiaodaor@users.noreply.github.com>
2019-05-01 16:27:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,25 +16,3 @@ if (typeof (Storage) !== "undefined") {
 } else {
     document.getElementById("result").innerHTML = "你这个浏览器不支持 Web Storage";
 }
-
-$(document).ready(function(){
-//Random pics
-var images = [
-	"https://i.loli.net/2019/03/15/5c8b0f6cb01ee.gif",
-	"https://i.loli.net/2019/03/15/5c8b0f568cacb.gif",
-	"https://i.loli.net/2019/03/15/5c8b0f60854a2.gif",
-	"https://i.loli.net/2019/03/15/5c8b0f624b2c4.gif",
-	"https://i.loli.net/2019/03/15/5c8b0f67acc24.gif",
-	"https://i.loli.net/2019/03/15/5c8b0f6b80e17.gif",
-	"https://i.loli.net/2019/03/15/5c8b0f6c11f2b.gif",
-	"https://i.loli.net/2019/03/15/5c8b116108485.gif",
-	"https://i.loli.net/2019/04/07/5ca8ecbc9ac19.gif",
-	"https://i.loli.net/2019/04/07/5ca8ecd2e96d9.gif",
-	"https://i.loli.net/2019/04/07/5ca8ecd332e33.gif",
-	"https://i.loli.net/2019/04/07/5ca8f253cf17f.gif",
-	"https://i.loli.net/2019/04/07/5ca8f358438d5.gif"
-	];
-var url = Math.floor(Math.random() * images.length);
-document.getElementById('img').src = images[url];
-});
-
