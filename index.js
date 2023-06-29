9c5499a4b2fbd6fa3c6ae7ac0062dfb7fa3d59aa
todhan <14990896+todhan@users.noreply.github.com>
2023-06-29 21:49:05 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-var d = new Date();
-var hour = d.getHours();
-if (hour > 0 && hour < 5) {
-    document.getElementsByClassName("hint")[0].style.display = "block";
-}
-
 // Random pics
 var bgArr = ["https://s2.ax1x.com/2020/03/01/36o3uD.gif", "https://s2.ax1x.com/2020/03/01/36oN4I.gif", "https://s2.ax1x.com/2020/03/01/36osbQ.gif", "https://s2.ax1x.com/2020/03/01/36orDg.gif", "https://s2.ax1x.com/2020/03/01/36TcLD.gif", "https://s6.jpg.cm/2022/05/14/LM5w0t.gif", "https://s2.ax1x.com/2020/03/01/36TyQK.gif", "https://s2.ax1x.com/2020/03/01/36TDRx.gif", "https://s2.ax1x.com/2020/03/01/36TRdH.gif", "https://s2.ax1x.com/2020/03/01/36T4JI.gif", "https://s1.ax1x.com/2020/03/20/86BbqJ.gif"];
 var bgIndex = Math.floor((Math.random() * bgArr.length));
 document.body.style.backgroundImage = "url(" + bgArr[bgIndex] + ")";
+
+// Late night reminder
+function displayMessage() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+
+    if (hours >= 0 && hours < 6) {
+        var link = document.createElement("a");
+        link.href = "https://music.163.com/#/song?id=29600147";
+        // 设置链接目标地址
+        link.textContent = "夜深了";
+        // 设置链接显示的文本内容
+        link.target = "_blank";
+        // 在新标签页中打开链接
+        link.classList.add("hint");
+        // 添加类名 "hint"
+        document.body.appendChild(link);
+        // 将链接添加到页面的body元素中
+    }
+}
+
+setInterval(displayMessage, 60000);
+// 每分钟检查一次时间
 
 // Switch the Tabs
 var btnArr = document.getElementsByTagName("button");
 var divArr = document.getElementsByClassName("con");
 for (var i = 0; i < btnArr.length; i++) {
     btnArr[i].index = i;
-    btnArr[i].onmouseover = function () {
+    btnArr[i].onmouseover = function() {
         for (var j = 0; j < btnArr.length; j++) {
             btnArr[j].className = "";
             divArr[j].style.display = "none"
@@ -28,13 +46,13 @@ for (var i = 0; i < btnArr.length; i++) {
 var d = document;
 // Check browser support
 if (typeof (Storage) !== "undefined") {
-    d.addEventListener("DOMContentLoaded", function () {
+    d.addEventListener("DOMContentLoaded", function() {
         var savedContent = localStorage.getItem("notesContent");
         if (savedContent != null) {
             d.getElementById("notes").value = savedContent;
         }
 
-        d.getElementById("notes").onkeyup = function () {
+        d.getElementById("notes").onkeyup = function() {
             var data = d.getElementById("notes").value;
             localStorage.setItem("notesContent", data);
         }
