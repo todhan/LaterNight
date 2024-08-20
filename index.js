14526f15ad441c43910bf50bfeddc131c2756fdb
todhan <14990896+todhan@users.noreply.github.com>
2024-08-21 02:48:57 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -208,13 +208,10 @@ const shortcuts = {
     78: ["https://www.notion.so/login"],
     71: ["https://github.com/login"],
     32: ["https://chat.openai.com/"],
-    88: ["https://x.com/"],
-    68: ["https://ddys.pro"],
-    90: ["https://www.zxzjhd.com"],
+    68: ["https://www.douban.com/favicon.ico"],
     89: ["https://www.youtube.com/?gl=US"],
     66: ["https://www.bilibili.com/"],
-    83: ["https://open.spotify.com/"],
-    65: ["https://movie.douban.com/mine?status=wish"]
+    83: ["https://open.spotify.com/"]
 };
 
 let isNotesFocused = false;
